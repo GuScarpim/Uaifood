fe4d7f18e191b3cf2455fbcbd0901067680306f6
guscarpim <gustavoscarpim@gmail.com>
2021-01-09 00:59:53 -0300

    Organização das pastas

---
+++ front/src/pages/landing/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import * as S from './styles';
+
+import Background from '../../assets/uaifood/bg.jpg';
+
+export default function ScrollAnimated() {
+
+  return (
+    <S.Container bg={Background}>
+    
+    </S.Container >
+  );
+}
