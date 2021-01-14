3c554391e22a9200e171090c350c43f1f2852d6c
Gustavo Scarpim <gustavoscarpim@gmail.com>
2021-01-14 01:04:55 -0300

    Filtro de custo

--- front/src/components/search/index.js
+++ front/src/components/search/index.js
@@ -14,7 +14,6 @@ export default function Search(props) {
   const [titleState, setTitleState] = useState('')
   const [cityName, setCityName] = useState('')
   const [searchValue, setSearchValue] = useState('')
-  const [countryId, setCountryId] = useState(null)
   const [open, setIsopen] = useState(false)
 
   let history = useHistory();
@@ -25,7 +24,6 @@ export default function Search(props) {
         .then((response) => {
           setTitleState(response.data.location_suggestions[0].title)
           setCityName(response.data.location_suggestions[0].city_name)
-          setCountryId(response.data.location_suggestions[0].city_id)
           console.log(response.data)
         }).catch((error) => {
           console.log(error)
@@ -40,12 +38,11 @@ export default function Search(props) {
 
   const SearchRestaurant = () => {
     localStorage.setItem('searchValue', searchValue)
-    localStorage.setItem('countryId', countryId)
     if (props.getRestaurants) {
       props.getRestaurants()
     }
     {
-      countryId !== null &&
+      searchValue !== null &&
       history.push(Globals.paths.restaurants)
     }
 
@@ -59,7 +56,7 @@ export default function Search(props) {
 
   return (
     <S.Container>
-      <S.PositionSearch disabled={countryId !== null && true}>
+      <S.PositionSearch disabled={searchValue !== null && true}>
         <input type="search" placeholder="Digite a sua cidade" onKeyUp={SearchCity}
           onChange={(e) => onChange(e)} value={searchValue} />
 
--- front/src/pages/restaurants/index.js
+++ front/src/pages/restaurants/index.js
@@ -14,27 +14,51 @@ import Globals from '../../Globals.json';
 
 export default function Restaurants() {
   const [stars] = useState([{ "id": 1, "stars": [0] }, { "id": 2, "stars": [0, 1] }, { "id": 3, "stars": [0, 1, 2] }, { "id": 4, "stars": [0, 1, 2, 3] }, { "id": 5, "stars": [0, 1, 2, 3, 4] }])
-  const [cust] = useState([{ id: 1, text: "Até R$50" }, { id: 2, text: "De R$50 a R$80" }, { id: 3, text: "De R$180 a R$110" }, { id: 4, text: "Acima de R110" }])
+  const [cust] = useState([{ id: 1, text: "Até R$50" }, { id: 2, text: "De R$50 a R$80" }, { id: 3, text: "De R$80 a R$110" }, { id: 4, text: "Acima de R$110" }])
   const [typeMeet] = useState(["Árabe", "Brasileira", "Chinesa", "Francesa", "Frutos do mar", "Italiana", "Japonesa", "Mexicana", "Peruana"])
-  const [restautants, setRestautants] = useState([])
+  const [restaurants, setRestaurants] = useState([])
+  const [orderBy, setOrderBy] = useState('Até R$50')
 
   useEffect(() => {
-    getRestautants()
+    getRestaurants()
   }, [])
 
-  const getRestautants = () => {
-    const countryId = localStorage.getItem('countryId');
+  const getRestaurants = (order, e) => {
     const city = localStorage.getItem('searchValue')
-    // api.get(Globals.api.collections + `?city_id=${countryId}`).then(response => {
-    //   setRestautants(response.data.collections)
-    //   console.log('Collections', restautants)
-    // }).catch(err => {
-    //   console.log(err)
-    // })
+    if (e) {
+      setOrderBy(e.target.value)
+    }
     api.get(Globals.api.search + `?entity_type=city&q=${city}`).then(response => {
-      setRestautants(response.data.restaurants)
-      // setCost(response.data.restaurants.average_cost_for_two)
-      console.log('Collections', restautants)
+      console.log(orderBy)
+        if (e) {
+          if (e.target.value === "Até R$50") {
+            console.log('entrou no"Até R$50" ')
+            var orderBy1 = response.data.restaurants.filter(item => item.restaurant.average_cost_for_two <= 50)
+            return setRestaurants(orderBy1)
+            // console.log('saiu do "Até R$50"', orderBy1, restaurants)
+          } else
+          if (e.target.value === "De R$50 a R$80") {
+            console.log('entrou no "De R$50 a R$80"')
+            var orderBy2 = response.data.restaurants.filter(item => item.restaurant.average_cost_for_two > 50 && item.restaurant.average_cost_for_two <= 80)
+            return setRestaurants(orderBy2)
+            // console.log('saiu do "De R$50 a R$80"', orderBy2, restaurants)
+          } else
+          if (e.target.value === "De R$80 a R$110") {
+            console.log('entrou no "De R$80 a R$110"')
+            var orderBy3 = response.data.restaurants.filter(item => item.restaurant.average_cost_for_two > 80 && item.restaurant.average_cost_for_two <= 110)
+            return setRestaurants(orderBy3)
+            // console.log('saiu do "De R$80 a R$110"', orderBy3, restaurants)
+          } else
+          if (e.target.value === "Acima de R$110") {
+            console.log('entrou no "Acima de R$110"')
+            var orderBy4 = response.data.restaurants.filter(item => item.restaurant.average_cost_for_two > 110)
+            return setRestaurants(orderBy4)
+            // console.log('saiu do "Acima de R$110"', orderBy4, restaurants)
+          }
+        } else {
+          var orderBy1 = response.data.restaurants.filter(item => item.restaurant.average_cost_for_two <= 50)
+          return setRestaurants(orderBy1)
+        }
     }).catch(err => {
       console.log(err)
     })
@@ -44,7 +68,7 @@ export default function Restaurants() {
     <>
       <S.Header>
         <img src={Logo} alt="Uaifood" />
-        <SearchInput getRestaurants={() => getRestautants()} />
+        <SearchInput getRestaurants={() => getRestaurants()} />
       </S.Header>
 
       <S.Container>
@@ -68,7 +92,7 @@ export default function Restaurants() {
 
             {cust.map(item => (
               <S.ContentsNote key={item.id}>
-                <input type="checkbox" id={item.id} />
+                <input type="checkbox" checked={orderBy === item.text ? true : false} onChange={(e) => getRestaurants(true, e)} value={item.text} />
                 <span>{item.text}</span>
               </S.ContentsNote>
             ))}
@@ -77,7 +101,7 @@ export default function Restaurants() {
 
             {typeMeet.map((item, index) => (
               <S.ContentsNote key={index}>
-                <input type="checkbox" />
+                <input type="checkbox" value={item} />
                 <span>{item}</span>
               </S.ContentsNote>
             ))}
@@ -91,9 +115,9 @@ export default function Restaurants() {
             <label className="title_restaurant">Restaurantes em {localStorage.getItem('searchValue')}</label>
 
             <S.FlexCard>
-              {restautants.map((item, index) => (
+              {restaurants.map((item, index) => (
                 <div key={index}>
-                  <Card image_url={item.restaurant.thumb} title={item.restaurant.name} link={item.restaurant.menu_url}
+                  <Card image_url={item.restaurant.thumb} title={item.restaurant.name} link={item.restaurant.menu_url || ''}
                     cost={item.restaurant.average_cost_for_two} cuisines={item.restaurant.cuisines} />
                 </div>
               ))}
